f86e353d891685ef4a36f8cb8a2c8aa64bc88250
Anderson Macias <ajo.macias@yavirac.edu.ec>
2022-10-01 20:35:19 -0500

    ..

--- src/components/skill/componets/HelperMessage.tsx
+++ src/components/skill/componets/HelperMessage.tsx
@@ -3,11 +3,11 @@ import { motion } from "framer-motion";
 function HelperMessage({ isInView }:HelperMessageProps) {
     return <motion.div
       style={{
-        opacity: isInView ? 1 : 0,
+        display : isInView ? 'flex' : 'none',
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
       }}
   
-      className="h-16 w-56 flex items-center rounded leading-4 p-2 fixed z-30 right-2 bottom-2 shadow-md bg-white "
+      className="h-16 w-56 items-center rounded leading-4 p-2 z-30 fixed right-2 bottom-2 shadow-md bg-white "
     >
       <p className="text-center text-gray-600 text-xs">Presiona en los iconos para ver lo que e hecho con ellas :)</p>
   
