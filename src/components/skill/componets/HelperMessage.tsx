78fc230b7835be5e01a4c08d29fc3242c9a492ef
Anderson Macias <ajo.macias@yavirac.edu.ec>
2022-10-01 22:36:07 -0500

    _

--- src/components/skill/componets/HelperMessage.tsx
+++ src/components/skill/componets/HelperMessage.tsx
@@ -8,9 +8,9 @@ function HelperMessage({ isInView }:HelperMessageProps) {
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
       }}
   
-      className="h-16 w-56 items-center rounded leading-4 p-2 z-30 flex fixed left-2 bottom-2 shadow-md bg-white "
+      className="h-16 w-56 text-white items-center rounded leading-4 p-2 z-30 flex fixed left-2 bottom-2 shadow-md"
     >
-      <p className="text-center text-gray-600 text-xs">Presiona en los iconos para ver lo que e hecho con ellas :)</p>
+      <p className="text-center text-xs">Presiona en los iconos para ver lo que e hecho con ellas :)</p>
   
     </motion.div>;
 }
