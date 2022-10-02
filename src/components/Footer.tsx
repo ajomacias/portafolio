9272ccecfd564fdb1bce88897196164021f20ac3
Anderson Macias <ajo.macias@yavirac.edu.ec>
2022-10-01 20:18:53 -0500

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = ()=>{
     return(
         <footer>
             <div className="flex flex-col items-center py-2 bg-gradient-to-l from-violet-800 to-emerald-600" >
-            <a className='h-5' target='blank' href="https://github.com/ajomacias">
+            <a className='h-5' href="https://github.com/ajomacias">
                 <AiFillGithub className='h-full w-full'/>
             </a>
             <p className="text-sm" >anderK222 | 2022</p>
